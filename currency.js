36eb62fceca39ad8eaa97daea15e07e2caacddeb
SengulC <sengulcagdal@gmail.com>
2025-10-05 16:30:40 +0100

    added html UI for currency converter

--- currency.js
+++ currency.js
@@ -1,13 +1,34 @@
-let amount = 10;
-let inputCur = "eur"
-let outputCur = "usd"
+let amount = document.getElementById("amount");
+let currencies = ["eur", "usd"];
+let inputCur = document.querySelector('input[name="fromCurrency"]:checked').value;
+let outputCur = inputCur == "eur" ? "usd" : "eur";
 
-function convert (amount, iCurrency, oCurrency) {
-    // if input crr is eur.. convert to usd
-    if (iCurrency == "eur")
-        return amount * 1.17;
-    else amount / 1.17;
-    // else do opposite
+function changeCur() {
+    inputCur = document.querySelector('input[name="fromCurrency"]:checked').value;
+    outputCur = inputCur == "eur" ? "usd" : "eur";
+    let obj = document.getElementsByClassName("outCur");
+    Object.entries(obj).map(entry => {
+        for (let i=0; i<entry.length; i++) {
+            entry[i].innerHTML = outputCur == "eur" ? "Euros" : "USD";
+        }
+    });
+    convert();
 }
 
-console.log(convert(amount, inputCur, outputCur))
+function convert () {
+    let output = document.getElementById("output");
+    // if input crr is eur.. convert to usd
+    if (inputCur == "eur") {
+        var val = (amount.value * 1.17);
+        output.innerHTML = parseFloat(val).toFixed(2);
+        console.log(amount.value * 1.17);
+    }
+    else { 
+        var val = (amount.value / 1.17);
+        output.innerHTML = parseFloat(val).toFixed(2);
+        console.log(amount.value / 1.17);
+    // else do opposite
+    }
+}
+
+// console.log(convert(amount, inputCur, outputCur))
